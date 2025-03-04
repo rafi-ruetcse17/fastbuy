20be175f15b538a2f19c2b01c4e9c830cfd87238
rafi <rafialam6610@gmail.com>
2025-03-04 20:03:46 +0600

    [FE] Fix client run related issue.

--- client/src/components/common/web-navbar/WebNavbar.js
+++ client/src/components/common/web-navbar/WebNavbar.js
@@ -3,24 +3,34 @@
 import { useSession } from "@/hooks/useSession";
 import styles from "./WebNavabr.module.css";
 import { Button } from "../button/Button";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { arePathsEqual } from "@/lib/utils/CommonUtils";
 import { appRouteList } from "@/lib/utils/PageRouteUtils";
 
 const WebNavbar = () => {
   const { removeSession } = useSession();
   const pathname = usePathname();
+  const router = useRouter();
 
   const handleLogout = () => {
     removeSession();
+  };
+
+  const handleRedirectionToUserPage = () => {
+    router.push(appRouteList.user);
   };
 
   return (
     <nav className={styles["nav"]}>
       <div></div>
       <div className={styles["nav-right"]}>
-        {!arePathsEqual(pathname, appRouteList.history) && (
-          <Button className={styles["history"]}>My History</Button>
+        {!arePathsEqual(pathname, appRouteList.user) && (
+          <Button
+            className={styles["history"]}
+            onClick={handleRedirectionToUserPage}
+          >
+            My porducts
+          </Button>
         )}
         <Button className={styles["logout-button"]} onClick={handleLogout}>
           LOGOUT
